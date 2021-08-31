9990372fa43f24ff7e4744b8e9387212918f2b27
Roman <79163288+besSmertniyKoder@users.noreply.github.com>
2021-08-31 21:57:11 +0300

    autowired got fixed

--- hiber/dao/UserDaoImp.java
+++ hiber/dao/UserDaoImp.java
@@ -3,6 +3,7 @@ package hiber.dao;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
@@ -11,32 +12,37 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-    @Autowired
     private SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public void add(User user) {
 
-        sessionFactory.getCurrentSession().save(user);
+        sessionFactory.openSession().save(user);
 
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.openSession().createQuery("from User");
         return query.getResultList();
     }
-// если честно понятия не имею как,но оно все еще работает и я бооюсь это трогать
-@Override
-public User getUserByCar(String model, int series) {
-    String hql = "FROM User u LEFT JOIN FETCH u.car WHERE u.car.model = :model AND u.car.series = :series";
 
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
+    // если честно понятия не имею как,но оно все еще работает и я бооюсь это трогать
+    @Override
+    public User getUserByCar(String model, int series) {
+        String hql = "FROM User u LEFT JOIN FETCH u.car WHERE u.car.model = :model AND u.car.series = :series";
+
+        TypedQuery<User> query = sessionFactory.openSession().createQuery(hql);
         query.setParameter("model", model);
         query.setParameter("series", series);
         return query.getSingleResult();
 
 
-}
+    }
 }
--- hiber/model/User.java
+++ hiber/model/User.java
@@ -1,5 +1,6 @@
 package hiber.model;
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -60,5 +61,16 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email);
+    }
 }
